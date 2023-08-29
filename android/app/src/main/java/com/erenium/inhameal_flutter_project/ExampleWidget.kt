ccbb066fc526e8142995e4b1bb5a5a4fdd5881f7
Moon <59304977+mun9769@users.noreply.github.com>
2023-08-29 23:42:28 +0900

    #14 - string을 json으로 변환한 시도

--- android/app/src/main/java/com/erenium/inhameal_flutter_project/ExampleWidget.kt
+++ android/app/src/main/java/com/erenium/inhameal_flutter_project/ExampleWidget.kt
@@ -6,7 +6,9 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
 import es.antonborri.home_widget.HomeWidgetPlugin
-
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlinx.android.synthetic.main.*
 /**
  * Implementation of App Widget functionality.
  */
@@ -24,9 +26,31 @@ class ExampleWidget : AppWidgetProvider() {
 
                 val cafeName = widgetData.getString("cafe_name", null)
                 setTextViewText(R.id.cafe_name, cafeName ?: "No title set")
+
+//                val meals = widgetData.getString("meals", null)
+//                val jsonObj = JSONObject(meals)
+//                val mymap = jsonObj.toMap()
+
+                setTextViewText(R.id.cafe_name, meals ?: "No title set")
             }
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+
+        // create TextView
+    }
+
+    fun JSONObject.toMap(): Map<String, *> = keys().asSequence().associateWith {
+        when (val value = this[it])
+        {
+            is JSONArray ->
+            {
+                val map = (0 until value.length()).associate { Pair(it.toString(), value[it]) }
+                JSONObject(map).toMap().values.toList()
+            }
+            is JSONObject -> value.toMap()
+            JSONObject.NULL -> null
+            else            -> value
         }
     }
 }
