1b602198ca010daf408682405b1c3ffae55c569a
Moon <59304977+mun9769@users.noreply.github.com>
2023-08-29 22:01:50 +0900

    #14 - 안드로이드 위젯
    위젯 설정 파일
    위젯 구현
    위젯 프리뷰 사진(귀여운 원숭이)도 넣음

---
+++ android/app/src/main/java/com/erenium/inhameal_flutter_project/ExampleWidget.kt
@@ -0,0 +1,30 @@
+package com.erenium.inhameal_flutter_project
+
+import android.annotation.SuppressLint
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetPlugin
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class ExampleWidget : AppWidgetProvider() {
+    @SuppressLint("RemoteViewLayout")
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray,
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            // Get reference to SharedPreferences
+            val widgetData = HomeWidgetPlugin.getData(context)
+            val views = RemoteViews(context.packageName, R.layout.example_widget).apply {
+
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
