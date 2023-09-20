3607c9d24c9411aa2fe135c319c9f03da63ccede
Moon <59304977+mun9769@users.noreply.github.com>
2023-09-21 00:29:12 +0900

    #27 - 컬러 수정
    피그마에 사용되는 컬러로 변경

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  AppColors._(); // this basically makes it so you can't instantiate this class
+  static const Color skyBlue = Color.fromARGB(255, 0, 102, 179);
+  static const Color deepBlue = Color.fromARGB(255, 0, 102, 179);
 
-  static const Map<int, Color> orange = <int, Color>{
-    50: Color(0xFFFCF2E7),
-    100: Color(0xFFF8DEC3),
-    200: Color(0xFFF3C89C),
-    300: Color(0xFFEEB274),
-    400: Color(0xFFEAA256),
-    500: Color(0xFFE69138),
-    600: Color(0xFFE38932),
-    700: Color(0xFFDF7E2B),
-    800: Color(0xFFDB7424),
-    900: Color(0xFFD56217)
-  };
+  static const Color lightGray = Color.fromARGB(255, 249, 249, 249);
+  static const Color gray = Color.fromARGB(255, 239, 239, 239);
+  static const Color deepGray = Color.fromARGB(255, 136, 136, 136);
+
+
+  static const Color textBlack = Color.fromARGB(255, 55, 55, 55);
+  static const Color textWhite = Color.fromARGB(255, 239, 239, 239);
+  static const Color textTimeWhite = Color.fromARGB(255, 171, 171, 171);
+  static const Color dividerWhite = Color.fromARGB(255, 201, 201, 201);
 }
